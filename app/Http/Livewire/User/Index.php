712f149889ab347bbdfda0e8e4e27d3986455fbc
fawait626 <fawait626gmail.com>
2023-03-02 09:30:56 +0700

    UPDATE

--- app/Http/Livewire/User/Index.php
+++ app/Http/Livewire/User/Index.php
@@ -7,17 +7,9 @@ use Livewire\Component;
 
 class Index extends Component
 {
-    public $search = '';
-    public $limit = 10;
-
-    protected $queryString = ['search','limit'];
 
     public function render()
     {
-
-        $query = User::query();
-        $query = $query->where('name', 'like', '%' . $this->search . '%');
-        $query = $query->paginate($this->limit);
-        return view('livewire.user.index', compact('query'));
+        return view('livewire.user.index');
     }
 }
